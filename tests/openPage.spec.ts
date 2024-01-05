d64db1628e8974803f6b6917183af4a44124d51d
jloncar7 <josip.loncar2ck@gmail.com>
2024-01-05 14:02:15 +0100

    Dodavanje assertiona u prvi test

--- tests/openPage.spec.ts
+++ tests/openPage.spec.ts
@@ -3,4 +3,5 @@ import { test, expect } from '@playwright/test';
 //otvori nasu demo stranicu
 test('test', async ({ page }) => {
     await page.goto('https://www.demoblaze.com/index.html');
+    await expect(page).toHaveURL('https://www.demoblaze.com/index.html')
 });
