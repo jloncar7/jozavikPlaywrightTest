1ff263ba79d7151bf0b6a49e356438c4a4f51083
jloncar7 <josip.loncar2ck@gmail.com>
2024-01-05 14:07:21 +0100

    Dodavanje assertionsa

--- tests/openPage.spec.ts
+++ tests/openPage.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect } from '@playwright/test';
 
-//otvori nasu demo stranicu
+//otvori nasu demo stranicu, dodaj assertionse
 test('test', async ({ page }) => {
     await page.goto('https://www.demoblaze.com/index.html');
     await expect(page).toHaveURL('https://www.demoblaze.com/index.html')
+    await expect(page).toHaveTitle('STORE')
 });
