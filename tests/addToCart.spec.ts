e3bbe604e1ecb15594de523410d0308ae5a6a89f
jloncar7 <josip.loncar2ck@gmail.com>
2024-01-08 18:53:37 +0100

    Peglanja addToCart casea

--- tests/addToCart.spec.ts
+++ tests/addToCart.spec.ts
@@ -1,13 +1,18 @@
 import { test, expect } from '@playwright/test';
 
 test('Add to cart', async ({ page }) => {
+    // navigiraj na stranicu
     await page.goto('https://www.demoblaze.com/index.html');
+    // klikni na samsung s6
     await page.getByRole('link', { name: 'Samsung galaxy s6' }).click();
+    // klikni na add to cart
     await page.getByRole('link', { name: 'Add to cart' }).click();
+    // rijesi se pop-upa
     page.on('dialog', dialog => {
         dialog.accept();
     });
-    await page.getByRole('link', { name: 'Add to cart' }).click();
+    // klikni na cart
     await page.getByRole('link', { name: 'Cart', exact: true }).click();
-    await expect(page.getByText('Products')).toBeVisible();
+    // assertion za kraj
+    await expect(page.getByText('Samsung galaxy s6')).toBeVisible();
 });
