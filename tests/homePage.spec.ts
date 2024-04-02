310ce0e54e83515471f17efc3061118a3cf9496f
jloncar7 <josip.loncar2ck@gmail.com>
2024-04-02 12:56:15 +0200

    Done with Homepage POM, started work on LogIn/Logout cases

--- tests/homePage.spec.ts
+++ tests/homePage.spec.ts
@@ -14,34 +14,20 @@ test.describe('homepage test cases', () => {
     test('check product store logo redirects correctly', async ({ page }) => {
         const pruductStoreRedirect = new HomePage(page);
         await pruductStoreRedirect.gotoHomePage();
-        //odi na neku stranicu
-        await page.getByRole('link', { name: 'Monitors' }).click();
-        await page.getByRole('link', { name: 'ASUS Full HD' }).click();
-        //stisni home gumb
-        await page.getByRole('link', { name: 'PRODUCT STORE' }).click();
-        //potvrdi da smo na homepageu
-        await page.getByText('CATEGORIES').isVisible();
+        await pruductStoreRedirect.productStoreButtonRedirect();
     });
 
     test('check home button redirects correctly', async ({ page }) => {
-        const homeButtonRedirect = new HomePage(page);
-        await homeButtonRedirect.gotoHomePage();
-        await page.getByRole('link', { name: 'Laptops' }).click();
-        await page.getByRole('link', { name: 'MacBook Pro' }).click();
-        await page.getByRole('link', { name: 'Home' }).click();
-        await page.getByText('CATEGORIES').isVisible();
-        await expect.soft(page.getByText('MacBook air')).not.toBeVisible();
+        const homeButton = new HomePage(page);
+        await homeButton.gotoHomePage();
+        await homeButton.homeButtonRedirect();
 
     });
 
     test('check categories button redirects to home', async ({ page }) => {
         const categoriesButtonRedirect = new HomePage(page);
         await categoriesButtonRedirect.gotoHomePage();
-        await page.getByRole('link', { name: 'Monitors' }).click();
-        await page.getByRole('link', { name: 'CATEGORIES' }).click();
-        await page.getByText('Nexus 6').isVisible();
-        await expect.soft(page.getByText('MacBook air')).not.toBeVisible();
-
+        await categoriesButtonRedirect.categoriesButtonRedirect();
     });
 
     test('categories - check only phones are visible', async ({ page }) => {
@@ -53,31 +39,20 @@ test.describe('homepage test cases', () => {
     test('categories - check only laptops are visible', async ({ page }) => {
         const categoriesLaptopCheck = new HomePage(page);
         await categoriesLaptopCheck.gotoHomePage();
-        await page.getByRole('link', { name: 'Laptops' }).click();
-        await page.getByText('Sony vaio i5').isVisible();
-        await page.getByText('2017 Dell 15.6 Inch').isVisible();
-        await page.getByText('Sony xperia z5').isVisible();
-        await expect.soft(page.getByText('Nokia lumia 1520')).not.toBeVisible();
-        await expect.soft(page.getByText('Apple monitor 24')).not.toBeVisible();
+        await categoriesLaptopCheck.gotoLaptops();
+
 
     });
 
     test('categories - check only monitors are visible', async ({ page }) => {
         const categoriesMonitorCheck = new HomePage(page);
         await categoriesMonitorCheck.gotoHomePage();
-        await page.getByRole('link', { name: 'Monitors' }).click();
-        await page.getByText('Apple monitor 24').isVisible();
-        await page.getByText('ASUS Full HD').isVisible();
-        await expect.soft(page.getByText('Nokia lumia 1520')).not.toBeVisible();
-        await expect.soft(page.getByText('Iphone 6 32gb')).not.toBeVisible();
-
+        await categoriesMonitorCheck.gotoMonitors();
     });
 
     test('homepage css tests', async ({ page }) => {
-        // navigiraj na stranicu
         const homePageCSS = new HomePage(page);
         await homePageCSS.gotoHomePage();
-        await expect(page.locator('#tbodyid')).toHaveCSS('outline-color', 'rgb(134, 134, 136)');
-        await expect(page.locator('#footc')).toHaveCSS('background-image', 'linear-gradient(to right, rgb(135, 15, 93) 0%, rgb(135, 15, 93) 19%, rgb(63, 41, 133) 55%, rgb(40, 17, 115) 100%)');
+        await homePageCSS.homePageVisualCheck();
     });
 });
--- tests/pages/homePagePOM.ts
+++ tests/pages/homePagePOM.ts
@@ -5,6 +5,22 @@ export class HomePage {
     readonly phonesLink: Locator;
     readonly laptopsLink: Locator;
     readonly monitorsLink: Locator;
+    readonly categoriesButton: Locator;
+    readonly homeButton: Locator;
+    readonly galaxyS6: Locator;
+    readonly nexus6: Locator;
+    readonly macBookAir: Locator;
+    readonly appleMonitor: Locator;
+    readonly sonyVaioi5: Locator;
+    readonly dell2017: Locator;
+    readonly nokiaLumia: Locator;
+    readonly asusFullHD: Locator;
+    readonly iPhone6: Locator;
+    readonly gridLayout: Locator;
+    readonly homeFooter: Locator;
+    readonly productsButton: Locator;
+
+
 
 
     constructor(page: Page) {
@@ -13,21 +29,78 @@ export class HomePage {
         this.phonesLink = page.getByRole('link', { name: 'Phones' });
         this.laptopsLink = page.getByRole('link', { name: 'Laptops' });
         this.monitorsLink = page.getByRole('link', { name: 'Monitors' });
+        this.categoriesButton = page.getByText('CATEGORIES');
+        this.homeButton = page.getByRole('link', { name: 'Home' })
+        this.productsButton = page.getByRole('link', { name: 'PRODUCT STORE' });
+        this.galaxyS6 = page.getByText('Samsung galaxy s6');
+        this.nexus6 = page.getByText('Nexus 6');
+        this.macBookAir = page.getByText('MacBook air');
+        this.appleMonitor = page.getByText('Apple monitor 24');
+        this.sonyVaioi5 = page.getByText('Sony vaio i5');
+        this.dell2017 = page.getByText('2017 Dell 15.6 Inch');
+        this.nokiaLumia = page.getByText('Nokia lumia 1520');
+        this.asusFullHD = page.getByText('ASUS Full HD');
+        this.iPhone6 = page.getByText('Iphone 6 32gb');
+        this.gridLayout = page.locator('#tbodyid');
+        this.homeFooter = page.locator('#footc');
+
+
+
     }
 
     async gotoHomePage() {
         await this.page.goto('https://www.demoblaze.com/index.html');
         await expect(this.page).toHaveURL('https://www.demoblaze.com/index.html');
         await expect.soft(this.page).toHaveTitle('STORE');
-        await this.page.getByText('CATEGORIES').isVisible();
+        await this.categoriesButton.isVisible();
 
     }
 
     async gotoPhones() {
         await this.phonesLink.click();
-        await this.page.getByText('Samsung galaxy s6').isVisible();
-        await this.page.getByText('Nexus 6').isVisible();
-        await expect.soft(this.page.getByText('MacBook air')).not.toBeVisible();
-        await expect.soft(this.page.getByText('Apple monitor 24')).not.toBeVisible();
+        await this.galaxyS6.isVisible();
+        await this.nexus6.isVisible();
+        await expect.soft(this.macBookAir).not.toBeVisible();
+        await expect.soft(this.appleMonitor).not.toBeVisible();
+    }
+
+    async gotoLaptops() {
+        await this.laptopsLink.click();
+        await this.sonyVaioi5.isVisible();
+        await this.dell2017.isVisible();
+        await expect.soft(this.nokiaLumia).not.toBeVisible();
+        await expect.soft(this.appleMonitor).not.toBeVisible();
+    }
+
+    async gotoMonitors() {
+        await this.monitorsLink.click();
+        await this.appleMonitor.isVisible();
+        await this.asusFullHD.isVisible();
+        await expect.soft(this.nokiaLumia).not.toBeVisible();
+        await expect.soft(this.iPhone6).not.toBeVisible();
+    }
+
+    async productStoreButtonRedirect() {
+        await this.monitorsLink.click();
+        await this.asusFullHD.click();
+        await this.productsButton.click();
+        await this.categoriesButton.isVisible();
+    }
+
+    async homeButtonRedirect() {
+        await this.homeButton.click();
+        await this.categoriesButton.isVisible();
+    }
+
+    async categoriesButtonRedirect() {
+        await this.monitorsLink.click();
+        await this.categoriesButton.click();
+        await this.nexus6.isVisible();
+        await expect.soft(this.macBookAir).not.toBeVisible();
+    }
+
+    async homePageVisualCheck() {
+        await expect(this.gridLayout).toHaveCSS('outline-color', 'rgb(134, 134, 136)');
+        await expect(this.homeFooter).toHaveCSS('background-image', 'linear-gradient(to right, rgb(135, 15, 93) 0%, rgb(135, 15, 93) 19%, rgb(63, 41, 133) 55%, rgb(40, 17, 115) 100%)');
     }
 }
